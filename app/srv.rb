0ade0244ef302819f22cc86f23defff8e9c286cb
wasuken <wevorence@gmail.com>
2021-01-31 19:55:53 +0900

    commit.

--- app/srv.rb
+++ app/srv.rb
@@ -1,14 +1,24 @@
 require 'sinatra'
-require 'natto'
 require 'json'
-require 'digest'
 require 'nokogiri'
-require 'nkf'
+require 'rom'
+require 'rom-repository'
 
 NATTO = Natto::MeCab.new
 
 INDEX_DIR = './index/'
 AOZORA_DIR = '/aozorabunko/cards/**/files/'
+
+DB = Sequel.connect('mysql2://root:test@db/bluesky')
+
+
+def insert(params)
+  db.transaction do
+    docs = DB[:documents]
+    docs.multi_insert(params)
+  end
+end
+
 
 get '/search' do
   q, s = params['q'], params['s']
@@ -16,23 +26,17 @@ get '/search' do
 end
 
 get '/rebuild' do
-  files = Dir.glob("#{AOZORA_DIR}5*.html")[0..1600]
+  files = Dir.glob("#{AOZORA_DIR}5*.html")[0...100]
   total = files.size
-  files[0..100].each_with_index do |filepath, i|
-    f = nil
-    if File.read(filepath, 500).include?('charset=Shift_JIS')
-      f = File.open(filepath, mode = "rt:sjis:utf-8").read
-    else
-      f = File.read(filepath)
-    end
+  recs = files.map do |filepath|
+    f = File.open(filepath, mode = "rt:sjis:utf-8").read
+    text = doc.at_css("body").text
     doc = Nokogiri::HTML.parse(
       f,
       nil,
-    'UTF-8')
-    body = doc.at_css("body")
-    text = body.text
+      'UTF-8')
     puts "[#{i+1}/#{total}] size: #{text.size}, #{filepath}"
-
+    {filepath: filepath, content: text}
   end
-  "success"
+  insert(recs)
 end
